4b1e7bcd10c507b67f849f7bef34da37308377ee
Fernando Barboza <falandocomofernando@gmail.com>
2020-11-24 00:01:23 -0300

    added sum

--- src/main.c
+++ src/main.c
@@ -3,14 +3,18 @@
 
 // Prototypes
 void print_header(void);
+int print_options(void);
+int sum(int value1, int value2);
 
 
 int main()
 {
+    int a;
+
     print_header();
+    a = print_options();
 
-    printf("Hi!");
-
+    printf("ANSWER: %d", a);
 
     return 0;
 }
@@ -21,4 +25,35 @@ void print_header(void)
     printf("\n*********** CALCULATOR ***********\n");
     printf("\n**********************************\n");
     return;
+}
+
+int print_options(void)
+{
+	int option;
+	int number1, number2, result;
+
+	printf("Pick an option: \n");
+    printf("1 - Sum \n");
+    printf("2 - Sub \n");
+    printf("3 - Mult \n");
+    printf("4 - Div \n");
+    scanf("%d", &option);
+
+    if (option == 1){
+    	printf("Type the first value: \n");
+    	scanf("%d", &number1);
+    	printf("Type the second value: \n");
+    	scanf("%d", &number2);
+
+    	result = sum(number1, number2);
+
+        return result;
+    }
+
+    return result;
+}
+
+int sum(int value1, int value2)
+{
+	return value1 + value2;
 }

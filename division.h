96f5aa33251e44a4082eb4c2886c196674d6bfd4
Fernando Barboza <falandocomofernando@gmail.com>
2020-11-24 22:34:41 -0300

    added the rest of the math functions, removed src dir and started using different files for functions

---
+++ division.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int division(int value1, int value2)
+{
+	return value1 / value2;
+}
---
+++ main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sum.h"
+#include "sub.h"
+#include "multiplication.h"
+#include "division.h"
+
+
+// Prototypes
+void print_header(void);
+int print_options(void);
+
+int main()
+{
+    int selected_option;
+
+    print_header();
+    selected_option = print_options();
+
+    printf("The answer is: %d", selected_option);
+
+    return 0;
+}
+
+void print_header(void)
+{
+    system("cls");
+    printf("**********************************\n");
+    printf("*********** CALCULATOR ***********\n");
+    printf("**********************************\n");
+    return;
+}
+
+int print_options(void)
+{
+	int option;
+	int number1, number2, result;
+
+	printf("Pick an option: \n");
+    printf("1 - Sum \n");
+    printf("2 - Subtraction \n");
+    printf("3 - Multiplication \n");
+    printf("4 - Division \n");
+
+    scanf("%d", &option);
+    fflush(stdin);
+
+    if (option == 1){
+
+    	printf("Type the first value: \n");
+    	scanf("%d", &number1);
+
+    	printf("Type the second value: \n");
+    	scanf("%d", &number2);
+
+        fflush(stdin);
+
+    	result = sum(number1, number2);
+        
+        return result;
+    }
+
+    if (option == 2){
+    	printf("Type the first value: \n");
+    	scanf("%d", &number1);
+    	printf("Type the second value: \n");
+    	scanf("%d", &number2);
+
+        fflush(stdin);
+
+    	result = sub(number1, number2);
+        
+        return result;
+    }
+
+    if (option == 3){
+    	printf("Type the first value: \n");
+    	scanf("%d", &number1);
+    	printf("Type the second value: \n");
+    	scanf("%d", &number2);
+
+        fflush(stdin);
+
+    	result = multi(number1, number2);
+        
+        return result;
+    }
+
+    if (option == 4){
+    	printf("Type the first value: \n");
+    	scanf("%d", &number1);
+    	printf("Type the second value: \n");
+    	scanf("%d", &number2);
+
+        fflush(stdin);
+
+    	result = division(number1, number2);
+        
+        return result;
+    }
+
+    return result;
+}
+
+
